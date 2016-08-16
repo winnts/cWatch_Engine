f38cff94d092c7be8a55e7a33c793c69b59be562
Andrey Dyachenko <winnts@gmail.com>
2016-08-16 16:30:53 +0300

    requests to DB added

--- src/main/Scripts/fake_attack.sh
+++ src/main/Scripts/fake_attack.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 i=1
-for i in {1..100}
+for i in {1..2000}
 do
-    wget http://io-sol.com/?malvare=curl
-    wget http://coolkodi.com/?malvare=curl
-    wget http://global-8000.com/?malvare=curl
+    wget -O /dev/null -o /dev/null http://io-sol.com/?malvare=curl
+    wget -O /dev/null -o /dev/null http://coolkodi.com/?malvare=curl
+    wget -O /dev/null -o /dev/null http://global-8000.com/?malvare=curl
 done
